aa75d6a007fd213cdec8f7ec88e9b6156e102dcb
Mauricio Salazar <mau.time@gmail.com>
2018-03-12 14:14:24 -0500

    UI - Main layout improvement (responsive) Exchange Transactions - Display transaction date

--- src/app/pages/_main/_main.component.ts
+++ src/app/pages/_main/_main.component.ts
@@ -1,8 +1,9 @@
-import { OnInit, Component } from "@angular/core";
+import { OnInit, Component, ChangeDetectorRef, ViewChild } from "@angular/core";
 import { AuthorizationService } from "../../services/auth.service";
 import { Router } from "@angular/router";
 import { BaseComponent } from "../base.component";
-import { MatSnackBar } from "@angular/material";
+import { MatSnackBar, MatSidenav } from "@angular/material";
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
     selector: 'main', 
@@ -10,12 +11,26 @@ import { MatSnackBar } from "@angular/material";
     styleUrls: ['_main.component.css']
 })
 export class MainComponent extends BaseComponent implements OnInit{
-    constructor(private router: Router, snackBar: MatSnackBar){
+
+    mobileQuery: MediaQueryList;
+
+    @ViewChild('sideNav')
+    sideNav: MatSidenav;
+
+
+    constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, snackBar: MatSnackBar){
         super(snackBar);
+        this.mobileQuery = media.matchMedia('(max-width: 600px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
     }
     
     ngOnInit(){
         console.log('MainComponent#ngOnInit');
+    }
+
+    ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
     logout(){
@@ -23,4 +38,9 @@ export class MainComponent extends BaseComponent implements OnInit{
             this.router.navigate(['/login']);
         });
     }
+
+    toggleSideNav() {
+        this.sideNav.toggle();
+    }
+    private _mobileQueryListener: () => void;
 }
